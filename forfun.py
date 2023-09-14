f4836241cc92f29ce49e45ccf61ed8c08a0afec6
awbreehey <145056099+awbreehey@users.noreply.github.com>
2023-09-14 14:21:39 -0400

    Add files via upload

---
+++ forfun.py
@@ -0,0 +1,50 @@
+#class forfun():
+
+  
+run= True
+    
+while (run==True):
+
+        choice= input("What is your favorite animal? \n Bunny, cat, bird, or pig?\n")
+
+        if(choice== "bunny" or choice== "Bunny"):
+            print ("\n"
+                   "sniff sniff~~  (\_/)\n"
+                   "              ( 'x' )\n"
+                   "              c(\")(\")\n"
+                   )
+            run=False
+    
+        elif(choice== "cat" or choice== "Cat"):
+            print ("\n"
+                   "meow~~  /\_/\ \n"
+                   "       (=^.^=)\n"
+                   "        (\")(\")_/"
+                   )
+            run=False
+        
+        elif(choice== "bird" or choice== "Bird"):
+            print ("\n"
+                   "tweet~~  _____\n"
+                   "        (' v ')\n"
+                   "       ((_____))\n"
+                   "          ^ ^"
+                   
+                   )
+            run=False
+    
+        elif(choice== "pig" or choice== "Pig"):
+            print ("\n"
+                "oink~~  ^---^\n"
+                "       ( 'o' )\n"
+                "       ( u u )"
+                   )
+            run=False
+
+        else:
+            print("That was not an option try again.")
+            run=True
+            
+
+
+
